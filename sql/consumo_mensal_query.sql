9313a61a8a623338058e04e3b6cc94e5c966e457
cadico <cadico@DESKTOP-QJ4SB8B.localdomain>
2020-07-13 17:10:14 -0300

    Updated the sql queries date

--- sql/consumo_mensal_query.sql
+++ sql/consumo_mensal_query.sql
@@ -12,16 +12,11 @@ from
     ITMVTO_ESTOQUE.CD_UNI_PRO = UNI_PRO.CD_UNI_PRO  ---- estava no registro Original do Carlos - ou seja é necessário considerar a unidade do produto naquele lote de movimentacao
   )
   join MVTO_ESTOQUE on (
-    ITMVTO_ESTOQUE.CD_MVTO_ESTOQUE = MVTO_ESTOQUE.CD_MVTO_ESTOQUE
+    ITMVTO_ESTOQUE.CD_MVTO_ESTOQUE = MVTO_ESTOQUE.CD_MVTO_ESTOQUE AND
+    MVTO_ESTOQUE.TP_MVTO_ESTOQUE IN ('P', 'S')
   )
   join PRODUTO on (
     ITMVTO_ESTOQUE.CD_PRODUTO = PRODUTO.CD_PRODUTO
-  )
-  join COTA_SETOR on (    --- Não estamos apresentando cota - retiraria esse join - apenas consume recurso de maquina
-    PRODUTO.CD_PRODUTO = COTA_SETOR.CD_PRODUTO
-  )
-  join SETOR on (  --- idem para este Join ---- Não estamos apresentando esta informação - retiraria esse join - apenas consume recurso de maquina
-    COTA_SETOR.CD_SETOR = SETOR.CD_SETOR
   )
 where
   ITMVTO_ESTOQUE.CD_PRODUTO in (
@@ -31,5 +26,5 @@ where
       DBAMV.PRODUTO
     WHERE DBAMV.PRODUTO.CD_PRODUTO in (154614, 191400, 201375, 97100068, 97100069, 97100070, 33020103, 940183, 96100029, 5100643, 20800, 200557, 202800, 33002102, 33002101, 202606, 21011, 202592)
   )
-  AND MVTO_ESTOQUE.DT_MVTO_ESTOQUE > to_date('2020-01-01', 'yyyy-mm-dd')
+  AND MVTO_ESTOQUE.DT_MVTO_ESTOQUE > to_date('2020-03-16', 'yyyy-mm-dd')
   GROUP BY ITMVTO_ESTOQUE.CD_PRODUTO, PRODUTO.DS_PRODUTO, EXTRACT(month from MVTO_ESTOQUE.DT_MVTO_ESTOQUE), EXTRACT(year from MVTO_ESTOQUE.DT_MVTO_ESTOQUE)
